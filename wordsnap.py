9e76b0e382b3eaa144df084f57b441a7b4619add
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-21 22:19:06 -0400

    Update wordsnap.py

--- wordsnap.py
+++ wordsnap.py
@@ -1,31 +1,18 @@
-import pytesseract, language_check      
-from pil import Image     
+import pytesseract, pyperclip    
+from PIL import Image     
 
 def main(file):
     #file = 'substinces.PNG'
     # opening an image from the source path 
-    img = Image.open(file)
-    dot = file.index(".")
-
-    name = file[0:dot]
-
-    tool = language_check.LanguageTool('en-US')
+    img = Image.open()
 
     # path where the tesseract module is installed 
     pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
     # converts the image to result and saves it into result variable 
-    result = pytesseract.image_to_string(img)    
-    # write text in a text file and save it to source path    
-    with open(name + '.txt',mode ='w') as file:      
-        file.write(result)
+    result = pytesseract.image_to_string(img)
+
+    pyperclip.copy(result)
+
+    
         
-    with open(name + '.txt',mode ='r') as file:      
-        orig = file.read()
-
-    matches = tool.check(orig)
-    orig = orig.replace(orig, language_check.correct(orig, matches))
-
-    with open(name + '.txt',mode ='w') as file:      
-        file.write(orig)
-
-    return str(len(matches)) + " mistakes were found."
+    
