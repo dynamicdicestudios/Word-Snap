8fe231a3e684e53f99e05ff918843b6178e34211
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-22 18:10:51 -0400

    Changed name

---
+++ convert.py
@@ -0,0 +1,20 @@
+import pytesseract, pyperclip    
+from PIL import ImageGrab, Image   
+
+def convert():
+    try:
+        file = 'image.PNG'
+        # opening an image from the source path 
+        img = ImageGrab.grabclipboard()
+
+        img.save(file, 'PNG')
+
+        image = Image.open(file)
+        # path where the tesseract module is installed 
+        pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
+        # converts the image to result and saves it into result variable 
+        result = pytesseract.image_to_string(image)
+
+        pyperclip.copy(result)
+    except AttributeError:
+        return False
