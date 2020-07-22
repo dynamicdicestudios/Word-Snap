394809dc130e971125530ba9b12b9afc5399ac28
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-21 22:30:31 -0400

    First GUI

---
+++ window.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from wordsnap import main
+
+grab_text = False
+
+root = Tk()
+root.title("Word Snap")
+root.geometry("300x180+500+250")
+
+def stop():
+    grab_text = False
+
+def start():
+    grab_text = True
+    while grab_text:
+        main()
+
+Button(root, text='Stop', width =5,command=root.destroy).place(x=20,y=130)
+Button(root, text='Start',width=5, command=grab_text).place(x=230,y=130)
+
+mainloop()
