7679e7a9be78b160722eece8c66f0b4b897c4779
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-22 12:53:53 -0400

    Adjusting Layout

--- window.py
+++ window.py
@@ -7,24 +7,23 @@ root = Tk()
 root.title("Word Snap")
 root.geometry("300x180+500+250")
 
-warning = Label(root, text="*Ensure an image has been added to\n the clipboard",
-                  font=("time new roman",10), fg='red').place(x=40,y=110)
-warning.config(state=DISABLED)
+#warning.config(state=DISABLED)
 
 def start():
-    warning.config(state=DISABLED)
+    #warning.config(state=DISABLED)
     worked = main()
     if worked == False:
-        warning.config(state="normal")
+        warning = Label(root, text="*Ensure an image has been added to\n the clipboard",
+                  font=("time new roman",10), fg='red').place(x=40,y=105)
         
 
-Button(root, text='Convert', width=10,command=start).place(x=125,y=145)
+Button(root, text='Convert', width=10,command=start).place(x=110,y=145)
 
 Label(root,
       text="Add an image to the\n clipboard, then click start to\n have the image\
  on the clipboard\n replaced with the text on the image.",
       font=("time new roman",10),
-      fg='black').place(x=30,y=30)
+      fg='black').place(x=30,y=25)
 
 
 
