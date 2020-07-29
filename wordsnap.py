47a5a280fdc5b21ba0fee4cd662e23ad28f54563
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-29 17:57:18 -0400

    Updated warning and layout

--- wordsnap.py
+++ wordsnap.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from convert import convert
 
-grab_text = False
-
 root = Tk()
 root.title("Word Snap")
 root.geometry("300x180+500+250")
@@ -12,21 +10,29 @@ warning = Label(root, text="",
 warning.pack()
 warning.place(x=30,y=105)
 
+success = Label(root, text="",
+                  font=("time new roman",10), fg='green')
+success.pack()
+success.place(x=30,y=120)
+
 def start():
-    #warning.config(state=DISABLED)
+    success.configure(text="")
     worked = convert()
     if worked == False:
         warning.configure(text="*Ensure a new image has been added to\n the clipboard.")
+    elif worked == None:
+        warning.configure(text="*No text could be found on the image.")
     else:
         warning.configure(text="")
+        success.configure(text="Finished converting! Check your clipboard.")
         
 
 Button(root, text='Convert', width=10,command=start).place(x=110,y=145)
 
 Label(root,
       text="Add an image to the\n clipboard, then click convert to\n have the image\
- on the clipboard\n replaced with the text on the image.",
+ on the clipboard\n replaced with the text on the image.\n\nNote: The image cannot be a file.",
       font=("time new roman",10),
-      fg='black').place(x=40,y=25)
+      fg='black').place(x=40,y=5)
 
 mainloop()
